606c6393311372ca86c94a0dfddbf951d043cd84
Jishu Das <jishudas329@gmail.com>
2026-01-21 13:38:57 +0000

    remove the firebase logo from the browser tab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type {Metadata} from 'next';
 import { Toaster } from "@/components/ui/toaster"
 import './globals.css';
@@ -81,6 +82,9 @@ export const metadata: Metadata = {
     canonical: siteUrl,
   },
   sitemap: `${siteUrl}/sitemap.xml`,
+  icons: {
+    icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22></text></svg>',
+  },
 };
 
 export default function RootLayout({
